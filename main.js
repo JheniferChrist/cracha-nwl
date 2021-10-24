0f671f23a4de216937c23694939e58d42c89c5bb
JHENIFER CHRIST SOUZA <85563316+JheniferChrist@users.noreply.github.com>
2021-10-24 10:38:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const LinksSocialMedia = {
     userName.textContent = data.name
     userBio.textContent = data.bio
     userLink.href = data.html_url   
-    UserImage.src = data.avatar_url
+    userImage.src = data.avatar_url
     userLogin.textContent = data.login
   })
 }
